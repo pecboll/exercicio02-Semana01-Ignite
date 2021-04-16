17c740747f0907975a2a02d078141ce254427e59
Igor Rocha <pecboll@gmail.com>
2021-04-16 10:35:42 -0300

    exercicio completo

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,26 @@
+import { Button } from '../components/Button';
+import { useSideBar } from '../hooks/UseSideBar';
+
 export function SideBar() {
   // Complete aqui
+  const { selectedGenreId, genres, handleClickButton } = useSideBar();
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+    </div>
+  )
 }
